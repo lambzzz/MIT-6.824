83e82d22fb64ccd85226aba662a19e677f6e20ae
lambzzz <791545998@qq.com>
2024-06-20 21:51:59 +0800

    Lab 1: MapReduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,79 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"sync"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	status       int
+	taskidx      int
+	tasknum      int
+	nReduce      int
+	tasklists    []string
+	workinglists map[string]int
+	mu           sync.Mutex
+	done         bool
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) Applytask(args *Argstype, reply *Replytype) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-//
+	if c.done {
+		reply.Tasktype = "done"
+		return nil
+	}
+
+	if c.taskidx == c.tasknum {
+		reply.Tasktype = "none"
+
+	} else if c.status == 0 {
+		reply.Tasktype = "map"
+		reply.Filename = c.tasklists[c.taskidx]
+		reply.NReduce = c.nReduce
+		c.taskidx++
+		c.workinglists[reply.Filename] = 5
+
+	} else if c.status == 1 {
+		reply.Tasktype = "reduce"
+		reply.Filename = c.tasklists[c.taskidx]
+		reply.NReduce = c.nReduce
+		c.taskidx++
+		c.workinglists[reply.Filename] = 5
+	} else {
+		reply.Tasktype = "none"
+		fmt.Printf("error status\n")
+	}
+	return nil
+}
+
+func (c *Coordinator) Submittask(args *Argstype, reply *Replytype) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.workinglists, args.Filename)
+
+	return nil
+}
+
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +87,57 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	ret := false
 
 	// Your code here.
 
+	for key, value := range c.workinglists {
+		if value == 0 {
+			delete(c.workinglists, key)
+			c.tasklists = append(c.tasklists, key)
+			c.tasknum++
+		} else {
+			c.workinglists[key] = value - 1
+		}
+	}
+	if c.tasknum == c.taskidx && len(c.workinglists) == 0 {
+		if c.status == 0 {
+			c.status = 1
+			c.taskidx = 0
+			c.tasknum = c.nReduce
+			c.tasklists = make([]string, c.nReduce)
+			for i := 0; i < c.nReduce; i++ {
+				c.tasklists[i] = strconv.Itoa(i)
+			}
+		} else {
+			c.done = true
+			return true
+		}
+	}
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
+	c.status = 0
+	c.tasklists = files
+	c.taskidx = 0
+	c.tasknum = len(files)
+	c.workinglists = make(map[string]int)
+	c.nReduce = nReduce
+	c.mu = sync.Mutex{}
+	c.done = false
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,141 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"strings"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func coorisalive() bool {
+	sockname := coordinatorSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		return false
+	}
+	defer c.Close()
+	return true
+}
 
-//
+func execmaptask(mapf func(string, string) []KeyValue, reply *Replytype, args *Argstype) {
+	filename := reply.Filename
+	filenamehash := ihash(filename)
+	nReduce := reply.NReduce
+	intermediate := make([][]KeyValue, nReduce)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+	for _, kv := range kva {
+		reduceidx := ihash(kv.Key) % nReduce
+		intermediate[reduceidx] = append(intermediate[reduceidx], kv)
+	}
+
+	for i := 0; i < nReduce; i++ {
+		jsonData, err := json.Marshal(intermediate[i])
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+		intermediateFile, err := os.Create(fmt.Sprintf("mr-%d-%d", i, filenamehash))
+		if err != nil {
+			log.Fatalf("cannot create %v", fmt.Sprintf("mr-%d-%d", i, filenamehash))
+		}
+		intermediateFile.Write(jsonData)
+		intermediateFile.Close()
+	}
+
+	args.Filename = filename
+	*reply = Replytype{}
+	ok := call("Coordinator.Submittask", &args, &reply)
+	if ok {
+
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	args.Filename = ""
+}
+
+func execreducetask(reducef func(string, []string) string, reply *Replytype, args *Argstype) {
+	filename := "mr-" + reply.Filename + "-"
+	var reduceout, t []KeyValue
+	reducekv := make(map[string][]string)
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatalf("Failed to read directory: %s", err)
+	}
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), filename) {
+			jsonData, err := os.ReadFile(file.Name())
+			if err != nil {
+				log.Fatalf("cannot read %v", filename)
+			}
+			err = json.Unmarshal(jsonData, &t)
+			if err != nil {
+				log.Fatalf("JSON unmarshaling failed: %s", err)
+			}
+			for _, kv := range t {
+				reducekv[kv.Key] = append(reducekv[kv.Key], kv.Value)
+			}
+		}
+	}
+	for k, v := range reducekv {
+		output := reducef(k, v)
+		reduceout = append(reduceout, KeyValue{k, output})
+	}
+	sort.Sort(ByKey(reduceout))
+	outname := "mr-out-" + reply.Filename
+	outfile, _ := os.Create(outname)
+
+	for i := 0; i < len(reduceout); i++ {
+		fmt.Fprintf(outfile, "%v %v\n", reduceout[i].Key, reduceout[i].Value)
+	}
+	outfile.Close()
+
+	args.Filename = reply.Filename
+	*reply = Replytype{}
+	ok := call("Coordinator.Submittask", &args, &reply)
+	if ok {
+
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	args.Filename = ""
+}
+
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -35,14 +143,39 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
+	if !coorisalive() {
+		fmt.Printf("coordinator is not alive\n")
+		return
+	}
+	args := Argstype{}
+	reply := Replytype{}
+	for {
+		reply = Replytype{}
+		ok := call("Coordinator.Applytask", &args, &reply)
+		if ok {
+			if reply.Tasktype == "none" {
+
+			} else if reply.Tasktype == "map" {
+				execmaptask(mapf, &reply, &args)
+
+			} else if reply.Tasktype == "reduce" {
+				execreducetask(reducef, &reply, &args)
+
+			} else if reply.Tasktype == "done" {
+				break
+			} else {
+				fmt.Printf("error tasktype\n")
+			}
+		} else {
+			fmt.Printf("call failed!\n")
+		}
+	}
 
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +200,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
